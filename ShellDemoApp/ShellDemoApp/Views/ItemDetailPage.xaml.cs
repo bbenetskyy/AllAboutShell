3824f9c6a1417176a64d68e8f35c24a536fc81c4
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2020-10-11 08:51:39 +0200

    Add Default App

---
+++ ShellDemoApp/ShellDemoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ShellDemoApp.ViewModels;
+
+namespace ShellDemoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
